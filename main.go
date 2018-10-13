47c268ec0a33e739760cfc8621aa85ba1e82ca7e
JoelPagliuca <joelpagliuca@hotmail.com>
2018-10-13 23:06:15 +1100

    very basic cli, will need to look into ways to limit connected adb devices

--- main.go
+++ main.go
@@ -1,16 +1,42 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	sw "github.com/JoelPagliuca/cmandroid-server/go"
 )
 
+// VERSION program version
+var VERSION string
+
+// GITCOMMIT commit hash that we built from
+var GITCOMMIT string
+
+func usage() {
+	fmt.Println("cmandroid server")
+	fmt.Println("version:", VERSION, "commit:", GITCOMMIT)
+	fmt.Println("")
+	flag.PrintDefaults()
+}
+
 func main() {
-	log.Printf("Server started")
+	helpFlag := flag.Bool("h", false, "Display this help text")
+	port := flag.Int("p", 8080, "Port to run the server on")
+	flag.Parse()
+
+	if *helpFlag {
+		usage()
+		os.Exit(0)
+	}
+
+	log.Printf("Server started on port " + fmt.Sprint(*port))
 
 	router := sw.NewRouter()
 
-	log.Fatal(http.ListenAndServe(":8080", router))
+	portString := fmt.Sprintf(":%d", *port)
+	log.Fatal(http.ListenAndServe(portString, router))
 }
