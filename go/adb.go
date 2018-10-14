4b90ec0850747e70b4068f929bfb1a466fda6cad
JoelPagliuca <joelpagliuca@hotmail.com>
2018-10-14 22:27:44 +1100

    filled out my functions a bit more, won't be happy until I unit test

--- go/adb.go
+++ go/adb.go
@@ -1,20 +1,56 @@
 package cmandroid
 
 import (
+	"bytes"
 	"fmt"
 	"os/exec"
+	"strings"
 )
 
+// AdbBinary path to the adb binary
 var AdbBinary string
 
+// AdbRun run an adb command
+func AdbRun(device string, args []string) (string, error) {
+	var cmd *exec.Cmd
+	if device != "" {
+		argsWithDevice := append([]string{"-d", device}, args...)
+		cmd = exec.Command(AdbBinary, argsWithDevice...)
+	} else {
+		cmd = exec.Command(AdbBinary, args...)
+	}
+	var stdout bytes.Buffer
+	cmd.Stdout = &stdout
+	err := cmd.Run()
+	commandOutput := stdout.String()
+	return commandOutput, err
+}
+
+// AdbStartServer used to make sure the adb server is ready
+func AdbStartServer() {
+	cmd := exec.Command(AdbBinary, "start-server")
+	cmd.Run()
+}
+
 // AdbGetDevices `adb devices`, return status
-func AdbGetDevices() (ListOfDevices, int) {
-	return MockListOfDevices, 0
+func AdbGetDevices() (ListOfDevices, error) {
+	commandOutput, err := AdbRun("", []string{"devices"})
+	splitCommandOutput := strings.Split(commandOutput, "\n")
+	if splitCommandOutput[0] == "List of devices attached" {
+		var deviceIds []string
+		for _, deviceLine := range splitCommandOutput[1:] {
+			deviceId := strings.Split(deviceLine, "")
+			deviceIds = append(deviceId, deviceIds...)
+		}
+		return ListOfDevices{deviceIds}, nil
+	} else {
+		return ListOfDevices{}, err
+	}
 }
 
 // AdbTap send a tap event to the device
 func AdbTap(deviceId string, x int, y int) error {
-	cmd := exec.Command(AdbBinary, "-d", deviceId, "input", "tap", fmt.Sprint(x), fmt.Sprint(y))
-	err := cmd.Run()
+	args := []string{"input", "tap", fmt.Sprint(x), fmt.Sprint(y)}
+	_, err := AdbRun(deviceId, args)
 	return err
 }
