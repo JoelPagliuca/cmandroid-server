73dad3253bfbb6d5e208f9041724c49f4b77aa57
JoelPagliuca <JoelPagliuca@users.noreply.github.com>
2018-10-27 15:36:35 +1100

    better test coverage

--- go/adb_test.go
+++ go/adb_test.go
@@ -8,41 +8,42 @@ import (
 )
 
 // MockListOfDevices used for mocking purposes
-var listOfDevices1 = ListOfDevices{[]string{"F39C7C991AD0", "5D05814EF3F1", "DF172F869D0C"}}
-
-func TestAdbGetDevices(t *testing.T) {
-	mockAdb := &mocks.AdbInterface{}
-	mockAdb.On("Run", "", []string{"devices"}).Return(`List of devices attached
+var listOfDevices1String = `List of devices attached
 
 F39C7C991AD0	device
 5D05814EF3F1	device
-DF172F869D0C	device`, nil)
-	type args struct {
-		adb AdbInterface
-	}
-	tests := []struct {
-		name    string
-		args    args
-		want    ListOfDevices
-		wantErr bool
-	}{
-		{
-			"Normal scenario",
-			args{adb: mockAdb},
-			listOfDevices1,
-			false,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got, err := AdbGetDevices(tt.args.adb)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("AdbGetDevices() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("AdbGetDevices() = %v, want %v", got, tt.want)
-			}
-		})
-	}
+DF172F869D0C	device`
+var listOfDevices1 = ListOfDevices{[]string{"F39C7C991AD0", "5D05814EF3F1", "DF172F869D0C"}}
+var listOfDevices2String = `List of devices attached
+
+`
+var listOfDevices2 = ListOfDevices{}
+
+func TestAdbGetDevices(t *testing.T) {
+	t.Run("Normal scenario", func(t *testing.T) {
+		mockAdb := &mocks.AdbInterface{}
+		mockAdb.On("Run", "", []string{"devices"}).Return(listOfDevices1String, nil)
+		want := listOfDevices1
+		got, err := AdbGetDevices(mockAdb)
+		if err != nil {
+			t.Errorf("AdbGetDevices() error = %v", err)
+			return
+		}
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("AdbGetDevices() = %v, want %v", got, want)
+		}
+	})
+	t.Run("No devices", func(t *testing.T) {
+		mockAdb := &mocks.AdbInterface{}
+		mockAdb.On("Run", "", []string{"devices"}).Return(listOfDevices2String, nil)
+		want := listOfDevices2
+		got, err := AdbGetDevices(mockAdb)
+		if err != nil {
+			t.Errorf("AdbGetDevices() error = %v", err)
+			return
+		}
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("AdbGetDevices() = %v, want %v", got, want)
+		}
+	})
 }
